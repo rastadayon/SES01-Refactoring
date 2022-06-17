840c01b8888ec9755d52b4c78f72d78bf7e007d2
Rasta <rasta.tadayon1378@gmail.com>
2022-06-17 19:15:39 +0430

    Extract getGPA method and move to Student class

--- src/domain/EnrollCtrl.java
+++ src/domain/EnrollCtrl.java
@@ -38,20 +38,14 @@ public class EnrollCtrl {
 		int unitsRequested = 0;
 		for (CSE o : courses)
 			unitsRequested += o.getCourse().getUnits();
-		double points = 0;
-		int totalUnits = 0;
-        for (Map.Entry<Term, Map<Course, Double>> tr : transcript.entrySet()) {
-            for (Map.Entry<Course, Double> r : tr.getValue().entrySet()) {
-                points += r.getValue() * r.getKey().getUnits();
-                totalUnits += r.getKey().getUnits();
-            }
-		}
-		double gpa = points / totalUnits;
-		if ((gpa < 12 && unitsRequested > 14) ||
+        double gpa = s.getGPA();
+        if ((gpa < 12 && unitsRequested > 14) ||
 				(gpa < 16 && unitsRequested > 16) ||
 				(unitsRequested > 20))
 			throw new EnrollmentRulesViolationException(String.format("Number of units (%d) requested does not match GPA of %f", unitsRequested, gpa));
 		for (CSE o : courses)
 			s.takeCourse(o.getCourse(), o.getSection());
 	}
+
+
 }
--- src/domain/Student.java
+++ src/domain/Student.java
@@ -55,4 +55,17 @@ public class Student {
 	public String toString() {
 		return name;
 	}
+
+	public double getGPA() {
+		double points = 0;
+		int totalUnits = 0;
+		for (Map.Entry<Term, Map<Course, Double>> tr : transcript.entrySet()) {
+			for (Map.Entry<Course, Double> r : tr.getValue().entrySet()) {
+				points += r.getValue() * r.getKey().getUnits();
+				totalUnits += r.getKey().getUnits();
+			}
+		}
+		double gpa = points / totalUnits;
+		return gpa;
+	}
 }
